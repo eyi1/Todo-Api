d3cd2a1946538476ce549e113011d13b594ba789
github username <ej7729@gmail.com>
2019-07-01 11:52:58 -0400

    fix syntax error

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Todo, type: :model do
 
   describe "Validations" do 
     it { should validate_presence_of :title}
-    it { should validate_presence_of :created_by}s
+    it { should validate_presence_of :created_by}
   end
   
 end
