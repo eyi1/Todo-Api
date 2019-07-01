dfc383ccea3c71087396b5e463219de5e8e23a59
github username <ej7729@gmail.com>
2019-07-01 11:16:16 -0400

    add association and validation tests

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "Associations" do
+    it { should have_many(:items) }
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :created_by}
+  end
+  
 end
