7e07e80f544686a1ba94cf2a24fbd018f6fa44a4
github username <ej7729@gmail.com>
2019-07-02 22:12:22 -0400

    move commented code to the bottom

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # scope module: :v1, constraints: ApiVersion.new('v1', true) do
-  #   resources :todos do
-  #     resources :items
-  #   end
-  # end
-  # scope module: :v2, constraints: ApiVersion.new('v2') do
-  #   resources :todos, only: :index
-  # end
+  
   resources :todos, only: :index
 
   namespace :api do
@@ -25,5 +17,14 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  # scope module: :v1, constraints: ApiVersion.new('v1', true) do
+  #   resources :todos do
+  #     resources :items
+  #   end
+  # end
+  # scope module: :v2, constraints: ApiVersion.new('v2') do
+  #   resources :todos, only: :index
+  # end
 
 end
